3309a9c818d1a1835d54f4f29be5a752f6a36d20
zhe-eugene <85669381+zhe-eugene@users.noreply.github.com>
2021-08-20 08:33:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 http_response_code(201);
 header('Content-Type: application/json');
-echo json_encode('4fdea8e75f646bb83a34ee1757850c3e');
+echo json_encode('5a3e692004a59ef1e888e659390ff895');
 
 // header("HTTP/1.1 301 Moved Permanently");
 // header("Location: https://test-page-error.herokuapp.com/");
