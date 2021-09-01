8a22e273aecc93210c63712bfed9cbb0e25eb680
zhe-eugene <85669381+zhe-eugene@users.noreply.github.com>
2021-09-01 14:17:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-// if(1) {
-// http_response_code(500);
-// }
-// else {
-// http_response_code(422);
-// }
+if(1) {
+http_response_code(500);
+}
+else {
+http_response_code(422);
+}
 
-http_response_code(200);
-header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'message' => 'success']);
+// http_response_code(200);
+// header('Content-Type: application/json');
+// echo json_encode(['status' => 'ok', 'message' => 'success']);
 
 // http_response_code(201);
 // header('Content-Type: application/json');
