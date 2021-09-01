c0b7d2ba249bababee5075b5a3bc9fd17a91a83f
zhe-eugene <85669381+zhe-eugene@users.noreply.github.com>
2021-09-01 14:07:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-// if(1) {
-// http_response_code(500);
-// }
-// else {
-// http_response_code(422);
-// }
+if(1) {
+http_response_code(500);
+}
+else {
+http_response_code(422);
+}
 
 // http_response_code(200);
 // header('Content-Type: application/json');
@@ -14,5 +14,5 @@
 // header('Content-Type: application/json');
 // echo json_encode('bb5e4fbba3775550de8f408dddb69955');
 
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: https://test-page-error.herokuapp.com/");
+// header("HTTP/1.1 301 Moved Permanently");
+// header("Location: https://test-page-error.herokuapp.com/");
